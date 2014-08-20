53b5f3d7e4abddcfb7301128068cd98e9d5a0f9e
James Wilkinson <jw@jameswilko.com>
2014-08-20 20:58:56 +1000

    Removed left-over test command (jesus christ reddit)

--- GoonHUD/lua/ChatManager.lua
+++ GoonHUD/lua/ChatManager.lua
@@ -10,20 +10,5 @@ end
 Hooks:RegisterHook( "ChatManagerOnReceiveMessage" )
 function ChatManager._receive_message(this, channel_id, name, message, color, icon)
 	Hooks:Call( "ChatManagerOnReceiveMessage", channel_id, name, message, color, icon )
-
-	if message == "/flash" then
-
-		local success, err = pcall(function()
-			local detonate_pos = GetPlayerAimPos( managers.player:player_unit() )
-			local rotation = Rotation()
-			local flash_grenade = World:spawn_unit(Idstring("units/weapons/flash_grenade_quick/flash_grenade_quick"), detonate_pos, Rotation())
-			flash_grenade:base():activate(detonate_pos, 5)
-		end)
-		if not success then
-			Print("Error: " .. err)
-		end
-
-	end
-
 	this.orig._receive_message(this, channel_id, name, message, color, icon)
 end
