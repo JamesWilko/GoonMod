d56fdd00695b72aa051e528725b2bfd76355f555
James Wilkinson <jw@jameswilko.com>
2015-01-23 22:12:12 +1100

    Update to v25

--- GoonBase/goonbase.lua
+++ GoonBase/goonbase.lua
@@ -5,7 +5,7 @@
 
 if not _G.GoonBase then
 	_G.GoonBase = {}
-	GoonBase.Version = 24
+	GoonBase.Version = 25
 	GoonBase.GameVersion = "1.24.0"
 	GoonBase.LogFile = "GoonBase.log"
 	GoonBase.Path = "GoonBase/"
