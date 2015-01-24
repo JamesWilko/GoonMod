59ec1a98227c32b4bef7026f3954fabebba7ff13
Bruce Li <leilmyxwz@gmail.com>
2015-01-23 23:16:55 -0500

    Reversed modifications to EnemyManager.lua

--- GoonBase/lua/EnemyManager.lua
+++ GoonBase/lua/EnemyManager.lua
@@ -7,13 +7,13 @@ CloneClass( EnemyManager )
 
 Hooks:RegisterHook( "EnemyManagerPreUpdateCorpseDisposal" )
 function EnemyManager._upd_corpse_disposal(this)
---	Hooks:Call("EnemyManagerPreUpdateCorpseDisposal", this)
+	Hooks:Call("EnemyManagerPreUpdateCorpseDisposal", this)
 	this.orig._upd_corpse_disposal(this)
 end
 
 Hooks:RegisterHook("EnemyManagerInitEnemyData")
 function EnemyManager._init_enemy_data(self)
 	self.orig._init_enemy_data(self)
---	Hooks:Call("EnemyManagerInitEnemyData", self)
+    Hooks:Call("EnemyManagerInitEnemyData", self)
 end
 -- END OF FILE
