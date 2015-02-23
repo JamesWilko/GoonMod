3326e5de7759ca3fc188d8c8574acc89c7fe2c2e
James Wilkinson <jw@jameswilko.com>
2015-02-23 15:56:04 +1100

    Removed Hooks:PCalls from HUDManager

--- GoonMod/lua/HUDManager.lua
+++ GoonMod/lua/HUDManager.lua
@@ -1,24 +1,6 @@
 
 CloneClass( HUDManager )
 
-Hooks:RegisterHook("HUDManagerSetStaminaValue")
-function HUDManager.set_stamina_value(this, value)
-	this.orig.set_stamina_value(this, value)
-	Hooks:PCall("HUDManagerSetStaminaValue", this, value)
-end
-
-Hooks:RegisterHook("HUDManagerSetMaxStamina")
-function HUDManager.set_max_stamina(this, value)
-	this.orig.set_max_stamina(this, value)
-	Hooks:PCall("HUDManagerSetMaxStamina", this, value)
-end
-
-Hooks:RegisterHook("HUDManagerSetMugshotDowned")
-function HUDManager.set_mugshot_downed(this, id)
-	this.orig.set_mugshot_downed(this, id)
-	Hooks:PCall("HUDManagerSetMugshotDowned", this, id)
-end
-
 Hooks:RegisterHook("HUDManagerPreAddWaypoint")
 function HUDManager.add_waypoint(self, id, data)
 	local r = Hooks:ReturnCall("HUDManagerPreAddWaypoint", self, id, data)
