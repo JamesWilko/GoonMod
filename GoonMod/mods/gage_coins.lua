3d9bcaff5c6e9200323674426eaff99705c524b9
James Wilkinson <jw@jameswilko.com>
2015-03-18 01:00:54 +1100

    GageCoins will now be displayed on the main player info panel and in the inventory alongside cash

--- GoonMod/mods/gage_coins.lua
+++ GoonMod/mods/gage_coins.lua
@@ -67,3 +67,124 @@ Hooks:Add("GageAssignmentManagerOnMissionCompleted", "GageAssignmentManagerOnMis
 	end
 
 end)
+
+Hooks:Add("PlayerProfileGuiObjectPostInit", "PlayerProfileGuiObjectPostInit_" .. Mod:ID(), function( gui, ws )
+
+	if not ExtendedInv or not GageCoins or not gui._panel then
+		return
+	end
+	if not ExtendedInv:HasItem( GageCoins.CoinID ) then
+		return
+	end
+
+	local offshore_text = nil
+	for k, v in pairs( gui._panel:children() ) do
+		if v.text then
+			if string.match( tostring(v:text()), tostring(managers.experience:cash_string(managers.money:offshore())) ) then
+				offshore_text = v
+			end
+		end
+	end
+
+	if offshore_text then
+
+		local font = tweak_data.menu.pd2_small_font
+		local font_size = tweak_data.menu.pd2_small_font_size
+		local gage_coins_text = gui._panel:text({
+			text = gui:get_text("gm_exinv_gage_coin_plural") .. ": " .. ExtendedInv:GetItem( GageCoins.CoinID ).amount,
+			font_size = font_size,
+			font = font,
+			color = tweak_data.screen_colors.text
+		})
+		gui:_make_fine_text(gage_coins_text)
+		gage_coins_text:set_left( math.round(offshore_text:left()) )
+		gage_coins_text:set_top( math.round(offshore_text:bottom()) )
+
+		local skillpoints = managers.skilltree:points()
+		local skill_text, skill_icon, skill_glow
+
+		if skillpoints > 0 then
+
+			for k, v in pairs( gui._panel:children() ) do
+
+				if v.text then
+					local skillpoint_text = gui:get_text("menu_spendable_skill_points", { points = tostring(skillpoints) })
+					if string.match( tostring(v:text()), tostring(skillpoint_text) ) then
+						skill_text = v
+					end
+				end
+
+				if v.texture_name then
+					local tex_name = v:texture_name()
+					if tex_name == Idstring("guis/textures/pd2/shared_skillpoint_symbol") then
+						skill_icon = v
+					end
+					if tex_name == Idstring("guis/textures/pd2/crimenet_marker_glow") then
+						skill_glow = v
+					end
+				end
+
+			end
+
+			skill_text:set_top(math.round(gage_coins_text:bottom()))
+			if skill_icon then
+				skill_icon:set_center_y(skill_text:center_y() + 1)
+			end
+			if skill_glow then
+				skill_glow:set_center_y(skill_icon:center_y())
+			end
+
+		end
+
+	end
+
+end)
+
+Hooks:Add("WalletGuiObjectOnSetWallet", "WalletGuiObjectOnSetWallet_" .. Mod:ID(), function( panel, layer )
+
+	if not ExtendedInv or not GageCoins then
+		return
+	end
+
+	local gage_coins_icon = Global.wallet_panel:bitmap({
+		name = "gage_coins_icon",
+		texture = "guis/textures/pd2/shared_wallet_symbol"
+	})
+
+	local text = tostring( ExtendedInv:GetItem( GageCoins.CoinID ).amount )
+	local gage_coins_text = Global.wallet_panel:text({
+		name = "gage_coins_text",
+		text = text,
+		font_size = tweak_data.menu.pd2_small_font_size,
+		font = tweak_data.menu.pd2_small_font,
+		color = tweak_data.screen_colors.text
+	})
+
+	local money_icon = Global.wallet_panel:child("wallet_money_icon")
+	local money_text = Global.wallet_panel:child("wallet_money_text")
+	gage_coins_icon:set_leftbottom( money_text:right() + 8, Global.wallet_panel:h() - 2 )
+	gage_coins_text:set_left(gage_coins_icon:right() + 2)
+	gage_coins_text:set_center_y(gage_coins_icon:center_y())
+	gage_coins_text:set_y( math.round(gage_coins_icon:y() - 2) )
+
+end)
+
+Hooks:Add("WalletGuiObjectOnRefresh", "WalletGuiObjectOnRefresh_" .. Mod:ID(), function()
+
+	if Global.wallet_panel then
+
+		local text = tostring( ExtendedInv:GetItem( GageCoins.CoinID ).amount )
+		local gage_coins_icon = Global.wallet_panel:child("gage_coins_icon")
+		local gage_coins_text = Global.wallet_panel:child("gage_coins_text")
+		local money_icon = Global.wallet_panel:child("wallet_money_icon")
+		local money_text = Global.wallet_panel:child("wallet_money_text")
+
+		gage_coins_icon:set_leftbottom( money_text:right() + 8, Global.wallet_panel:h() - 2 )
+		gage_coins_text:set_text( text )
+		gage_coins_text:set_left(gage_coins_icon:right() + 2)
+		gage_coins_text:set_center_y(gage_coins_icon:center_y())
+		gage_coins_text:set_y( math.round(gage_coins_icon:y() - 2) )
+
+	end
+
+end)
