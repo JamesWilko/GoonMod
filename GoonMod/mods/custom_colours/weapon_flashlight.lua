e17dd4f4d486fda8dedd9d40574d86cba8b27bcf
James Wilkinson <jw@jameswilko.com>
2015-03-19 01:45:59 +1100

    Converted custom weapon flashlight and world laser colours to BLT Custom colour menu nodes now use json-loaded menu files Custom colour menu nodes have a colour sample space that updates in realtime

---
+++ GoonMod/mods/custom_colours/weapon_flashlight.lua
@@ -0,0 +1,213 @@
+
+-- Mod Definition
+local Mod = class( BaseMod )
+Mod.id = "CustomWeaponFlashlight"
+Mod.Name = "Custom Weapon Flashlight Colour"
+Mod.Desc = "Set a custom colour for flashlights attached to your weapons"
+Mod.Requirements = {}
+Mod.Incompatibilities = {}
+Mod.Priority = 1
+
+Hooks:Add("GoonBaseRegisterMods", "GoonBaseRegisterMutators_" .. Mod.id, function()
+	GoonBase.Mods:RegisterMod( Mod )
+end)
+
+if not Mod:IsEnabled() then
+	return
+end
+
+-- Weapon Light
+GoonBase.WeaponLight = GoonBase.WeaponLight or {}
+local Light = GoonBase.WeaponLight
+Light.MenuFile = "custom_flashlight_menu.txt"
+
+-- Options
+GoonBase.Options.WeaponLight 				= GoonBase.Options.WeaponLight or {}
+GoonBase.Options.WeaponLight.Enabled 		= GoonBase.Options.WeaponLight.Enabled or true
+GoonBase.Options.WeaponLight.RH 			= GoonBase.Options.WeaponLight.RH or 1
+GoonBase.Options.WeaponLight.GS 			= GoonBase.Options.WeaponLight.GS or 1
+GoonBase.Options.WeaponLight.BV 			= GoonBase.Options.WeaponLight.BV or 1
+GoonBase.Options.WeaponLight.UseHSV 		= GoonBase.Options.WeaponLight.UseHSV or false
+GoonBase.Options.WeaponLight.UseRainbow 	= GoonBase.Options.WeaponLight.UseRainbow or false
+GoonBase.Options.WeaponLight.RainbowSpeed 	= GoonBase.Options.WeaponLight.RainbowSpeed or 1
+
+-- Light Colour
+Light.Color = ColorHSVRGB:new( GoonBase.Options.WeaponLight, Color.white )
+
+-- Functions
+function Light:IsEnabled()
+	return GoonBase.Options.WeaponLight.Enabled
+end
+
+function Light:IsRainbow()
+	return GoonBase.Options.WeaponLight.UseRainbow
+end
+
+function Light:RainbowSpeed()
+	return GoonBase.Options.WeaponLight.RainbowSpeed
+end
+
+function Light:GetColor(alpha)
+	return Light.Color:GetColor( alpha )
+end
+
+function Light:ShowPreviewMenuItem()
+
+	if not managers.menu_component then
+		return
+	end
+
+	local ws = managers.menu_component._ws
+	self._panel = ws:panel():panel()
+
+	local w, h = self._panel:w() * 0.35, 48
+	self._color_rect = self._panel:rect({
+		w = w,
+		h = h,
+		color = Color.red,
+		blend_mode = "add",
+		layer = tweak_data.gui.MOUSE_LAYER - 50,
+	})
+	self._color_rect:set_right( self._panel:right() )
+	self._color_rect:set_top( self._panel:h() * 0.265 )
+
+	self:UpdatePreview()
+
+end
+
+function Light:DestroyPreviewMenuItem()
+
+	if alive(self._panel) then
+
+		self._panel:remove( self._color_rect )
+		self._panel:remove( self._panel )
+
+		self._color_rect = nil
+		self._panel = nil
+
+	end
+
+end
+
+function Light:UpdatePreview( t )
+
+	if not alive(self._panel) or not alive(self._color_rect) or not Light.Color then
+		return
+	end
+
+	if self:IsRainbow() and t then
+		self._color_rect:set_color( Light.Color:GetRainbowColor(t, self:RainbowSpeed()) )
+	else
+		self._color_rect:set_color( Light.Color:GetColor() )
+	end
+
+end
+
+-- Menu
+Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_" .. Mod:ID(), function( menu_manager )
+
+	-- Callbacks
+	MenuCallbackHandler.CustomFlashlightMenuChangeFocus = function( node, focus )
+		if focus then
+			Light:ShowPreviewMenuItem()
+		else
+			Light:DestroyPreviewMenuItem()
+		end
+	end
+
+	MenuCallbackHandler.ToggleEnableCustomFlashlight = function( this, item )
+		GoonBase.Options.WeaponLight.Enabled = item:value() == "on" and true or false
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightToggleUseHue = function( this, item )
+		GoonBase.Options.WeaponLight.UseHSV = item:value() == "on" and true or false
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightSetRedHue = function( this, item )
+		GoonBase.Options.WeaponLight.RH = tonumber( item:value() )
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightSetGreenSaturation = function( this, item )
+		GoonBase.Options.WeaponLight.GS = tonumber( item:value() )
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightSetBlueValue = function( this, item )
+		GoonBase.Options.WeaponLight.BV = tonumber( item:value() )
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightSetUseRainbow = function( this, item )
+		GoonBase.Options.WeaponLight.UseRainbow = item:value() == "on" and true or false
+		Light:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomFlashlightSetRainbowSpeed = function( this, item )
+		GoonBase.Options.WeaponLight.RainbowSpeed = tonumber( item:value() )
+	end
+
+	MenuHelper:LoadFromJsonFile( GoonBase.MenusPath .. Light.MenuFile, GoonBase.WeaponLight, GoonBase.Options.WeaponLight )
+
+end)
+
+-- Hooks
+Hooks:Add("MenuUpdate", "MenuUpdate_" .. Mod:ID(), function(t, dt)
+	if Light:IsRainbow() then
+		Light:UpdatePreview( t )
+	end
+end)
+
+Hooks:Add("GameSetupUpdate", "GameSetupUpdate_" .. Mod:ID(), function(t, dt)
+	if Light:IsRainbow() then
+		Light:UpdatePreview( t )
+	end
+end)
+
+Hooks:Add("WeaponFlashLightInit", "WeaponFlashLightInit_CustomLight", function(flashlight, unit)
+
+	if not Light:IsEnabled() then
+		Hooks:Remove("WeaponFlashLightInit_CustomLight")
+		return
+	end
+
+	flashlight._light:set_color( Light:GetColor() )
+
+end)
+
+Hooks:Add("WeaponFlashLightCheckState", "WeaponFlashLightCheckState_CustomLight", function(flashlight)
+
+	if flashlight._on then
+
+		flashlight._unit:set_extension_update_enabled( Idstring("base"), flashlight._on )
+
+		Hooks:RegisterHook("WeaponFlashLightUpdate")
+		flashlight._old_update = flashlight.update
+		flashlight.update = function(self, unit, t, dt)
+			Hooks:Call( "WeaponFlashLightUpdate", self, unit, t, dt )
+			if flashlight.overkill_update ~= nil then
+				flashlight.overkill_update(self, unit, t, dt)
+			end
+		end
+
+	end
+
+end)
+
+Hooks:Add("WeaponFlashLightUpdate", "WeaponFlashLightUpdate_Rainbow", function(flashlight, unit, t, dt)
+
+	if not Light:IsEnabled() then
+		return
+	end
+
+	if not Light:IsRainbow() then
+		flashlight._light:set_color( Light:GetColor() )
+	end
+
+	if Light:IsRainbow() then
+		flashlight._light:set_color( Light.Color:GetRainbowColor( t, Light:RainbowSpeed() ) )
+	end
+
+end)
---
+++ GoonMod/mods/custom_colours/world_laser_colors.lua
@@ -0,0 +1,211 @@
+
+-- Mod Definition
+local Mod = class( BaseMod )
+Mod.id = "CustomWorldLaserColour"
+Mod.Name = "Custom World Laser Colour"
+Mod.Desc = "Set a custom colour for lasers that appear in the game world"
+Mod.Requirements = {}
+Mod.Incompatibilities = {}
+Mod.Priority = 1
+
+Hooks:Add("GoonBaseRegisterMods", "GoonBaseRegisterMutators_" .. Mod.id, function()
+	GoonBase.Mods:RegisterMod( Mod )
+end)
+
+if not Mod:IsEnabled() then
+	return
+end
+
+-- Lasers
+GoonBase.WorldLasers = GoonBase.WorldLasers or {}
+local Lasers = GoonBase.WorldLasers
+Lasers.MenuFile = "custom_world_laser_menu.txt"
+Lasers._WorldOpacity = 0.4
+
+-- Options
+GoonBase.Options.WorldLasers 				= GoonBase.Options.WorldLasers or {}
+GoonBase.Options.WorldLasers.Enabled 		= GoonBase.Options.WorldLasers.Enabled or true
+GoonBase.Options.WorldLasers.RH 			= GoonBase.Options.WorldLasers.RH or 0.6
+GoonBase.Options.WorldLasers.GS 			= GoonBase.Options.WorldLasers.GS or 0.0
+GoonBase.Options.WorldLasers.BV 			= GoonBase.Options.WorldLasers.BV or 0.0
+GoonBase.Options.WorldLasers.UseHSV 		= GoonBase.Options.WorldLasers.UseHSV or false
+GoonBase.Options.WorldLasers.UseRainbow 	= GoonBase.Options.WorldLasers.UseRainbow or false
+GoonBase.Options.WorldLasers.RainbowSpeed 	= GoonBase.Options.WorldLasers.RainbowSpeed or 1
+
+-- Laser Colour
+Lasers.Color = ColorHSVRGB:new( GoonBase.Options.WorldLasers, Color.red:with_alpha(0.6) )
+
+-- Functions
+function Lasers:IsEnabled()
+	return GoonBase.Options.WorldLasers.Enabled
+end
+
+function Lasers:IsRainbow()
+	return GoonBase.Options.UseRainbow.Rainbow
+end
+
+function Lasers:GetColor(alpha)
+	if Lasers.Color == nil then
+		Lasers.Color = ColorHSVRGB:new()
+		Lasers.Color:SetOptionsTable( "WorldLasers" )
+	end
+	return Lasers.Color:GetColor( alpha ) or Color( alpha or 1, 1, 0, 0 )
+end
+
+
+function Lasers:IsEnabled()
+	return GoonBase.Options.WorldLasers.Enabled
+end
+
+function Lasers:IsRainbow()
+	return GoonBase.Options.WorldLasers.UseRainbow
+end
+
+function Lasers:RainbowSpeed()
+	return GoonBase.Options.WorldLasers.RainbowSpeed
+end
+
+function Lasers:GetColor(alpha)
+	return Lasers.Color:GetColor( alpha )
+end
+
+function Lasers:ShowPreviewMenuItem()
+
+	if not managers.menu_component then
+		return
+	end
+
+	local ws = managers.menu_component._ws
+	self._panel = ws:panel():panel()
+
+	local w, h = self._panel:w() * 0.35, 48
+	self._color_rect = self._panel:rect({
+		w = w,
+		h = h,
+		color = Color.red,
+		blend_mode = "add",
+		layer = tweak_data.gui.MOUSE_LAYER - 50,
+	})
+	self._color_rect:set_right( self._panel:right() )
+	self._color_rect:set_top( self._panel:h() * 0.265 )
+
+	self:UpdatePreview()
+
+end
+
+function Lasers:DestroyPreviewMenuItem()
+
+	if alive(self._panel) then
+
+		self._panel:remove( self._color_rect )
+		self._panel:remove( self._panel )
+
+		self._color_rect = nil
+		self._panel = nil
+
+	end
+
+end
+
+function Lasers:UpdatePreview( t )
+
+	if not alive(self._panel) or not alive(self._color_rect) or not Lasers.Color then
+		return
+	end
+
+	if self:IsRainbow() and t then
+		self._color_rect:set_color( Lasers.Color:GetRainbowColor(t, self:RainbowSpeed()) )
+	else
+		self._color_rect:set_color( Lasers.Color:GetColor() )
+	end
+
+end
+
+-- Menu
+Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_" .. Mod:ID(), function( menu_manager )
+
+	-- Callbacks
+	MenuCallbackHandler.CustomWorldLaserMenuChangeFocus = function( node, focus )
+		if focus then
+			Lasers:ShowPreviewMenuItem()
+		else
+			Lasers:DestroyPreviewMenuItem()
+		end
+	end
+
+	MenuCallbackHandler.ToggleEnableCustomWorldLaser = function( this, item )
+		GoonBase.Options.WorldLasers.Enabled = item:value() == "on" and true or false
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserToggleUseHue = function( this, item )
+		GoonBase.Options.WorldLasers.UseHSV = item:value() == "on" and true or false
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserSetRedHue = function( this, item )
+		GoonBase.Options.WorldLasers.RH = tonumber( item:value() )
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserSetGreenSaturation = function( this, item )
+		GoonBase.Options.WorldLasers.GS = tonumber( item:value() )
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserSetBlueValue = function( this, item )
+		GoonBase.Options.WorldLasers.BV = tonumber( item:value() )
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserSetUseRainbow = function( this, item )
+		GoonBase.Options.WorldLasers.UseRainbow = item:value() == "on" and true or false
+		Lasers:UpdatePreview()
+	end
+
+	MenuCallbackHandler.CustomWorldLaserSetRainbowSpeed = function( this, item )
+		GoonBase.Options.WorldLasers.RainbowSpeed = tonumber( item:value() )
+	end
+
+	MenuHelper:LoadFromJsonFile( GoonBase.MenusPath .. Lasers.MenuFile, GoonBase.WorldLasers, GoonBase.Options.WorldLasers )
+
+end)
+
+-- Hooks
+Hooks:Add("MenuUpdate", "MenuUpdate_" .. Mod:ID(), function(t, dt)
+	if Lasers:IsRainbow() then
+		Lasers:UpdatePreview( t )
+	end
+end)
+
+Hooks:Add("GameSetupUpdate", "GameSetupUpdate_" .. Mod:ID(), function(t, dt)
+	if Lasers:IsRainbow() then
+		Lasers:UpdatePreview( t )
+	end
+end)
+
+Hooks:Add("ElementLaserTriggerPostInit", "ElementLaserTriggerPostInit_WorldLaser", function(laser)
+
+	if not Lasers:IsEnabled() then
+		return
+	end
+
+	laser._brush:set_color( Lasers:GetColor(Lasers._WorldOpacity) )
+
+end)
+
+Hooks:Add("ElementLaserTriggerUpdateDraw", "ElementLaserTriggerUpdateDraw_WorldLaser", function(laser, t, dt)
+
+	if not Lasers:IsEnabled() then
+		return
+	end
+	
+	if not Lasers:IsRainbow() then
+		laser._brush:set_color( Lasers:GetColor(Lasers._WorldOpacity) )
+	end
+
+	if Lasers:IsRainbow() then
+		laser._brush:set_color( Lasers.Color:GetRainbowColor( t, Lasers:RainbowSpeed() ):with_alpha(Lasers._WorldOpacity) )
+	end
+
+end)
