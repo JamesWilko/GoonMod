ea02f6ba28ac0cc0d89f5e108d35098cb947a190
hexco321 <hexco321@users.noreply.github.com>
2017-02-22 13:08:53 +0000

    Fixes SkillTreeGUINew crash issue #141

--- GoonMod/lua/SkillTreeGUINew.lua
+++ GoonMod/lua/SkillTreeGUINew.lua
@@ -2,9 +2,9 @@
 CloneClass( NewSkillTreeSkillItem )
 
 Hooks:RegisterHook("NewSkillTreeSkillItemPostInit")
-function NewSkillTreeSkillItem.init(self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, fullscreen_panel, gui)
-	self.orig.init(self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, fullscreen_panel, gui)
-	Hooks:Call("NewSkillTreeSkillItemPostInit", self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, fullscreen_panel, gui)
+function NewSkillTreeSkillItem.init(self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, tier_item, fullscreen_panel, gui)
+	self.orig.init(self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, tier_item, fullscreen_panel, gui)
+	Hooks:Call("NewSkillTreeSkillItemPostInit", self, skill_id, skill_data, skill_panel, tree_panel, tree, tier, tier_item, fullscreen_panel, gui)
 end
 
 Hooks:RegisterHook("NewSkillTreeSkillItemPostRefresh")
